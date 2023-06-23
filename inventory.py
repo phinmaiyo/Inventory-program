eb51b7c289f249cd6479ecc3e39774ce57524f53
Phonex Chemutai <phinmaiyo@gmail.com>
2023-06-23 21:44:21 +0300

    code to add new student

--- inventory.py
+++ inventory.py
@@ -24,4 +24,13 @@ def student_inventory():                      # defines inventory function
             for key in _inventory:
                 print(key + ":", _inventory[key])
 
-    
+    elif (userInput == 2):                                             # adds new student
+        newstud = input("Please Enter the student name : ").lower()
+        stud_age = int(input("Please Enter student age : "))
+        stud_GPA = int(input("Please Enter student GPA : "))
+        stud_hobby = input("Please Enter student hobby : ")
+        if newstud in dictstd:
+            print(f"\n This student {newstud} already in inventory")
+        else:
+            dictstd[newstud] = {"Age": stud_age, "GPA": stud_GPA, "Hobby": stud_hobby}
+            print(f"\n This student {newstud} added successfully")
